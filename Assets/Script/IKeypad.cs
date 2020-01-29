7a9c61686e552e15c4bd5766427eef00dd5c3790
hitoiki <hitoiki103@gmail.com>
2020-01-30 00:46:21 +0900

    projectstart

---
+++ Assets/Script/IKeypad.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IKeypad
+{
+    bool Accel();
+    bool Handle();//floatで弄ってもいいかも
+}
+
---
+++ Assets/Script/RacerAccel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacerAccel : MonoBehaviour
+{
+    public IKeypad ikeypad;
+    public Rigidbody rb;
+
+    void FixedUpdate()
+    {
+        if (ikeypad.Accel())
+        {
+            rb.AddForce(Vector3.forward);
+        }
+    }
+
+}
